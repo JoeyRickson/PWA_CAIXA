631defff0232afb55004fbbba5907a51d7478bf8
JoeyRickson <joeyoliveira8@gmail.com>
2026-09-16 10:02:05 -0400

    Melhora fluxo e retencao de backups

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meu-caixa-v2.3';
+const CACHE='meu-caixa-v2.4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
