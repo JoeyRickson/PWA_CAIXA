fde093e2401a4b7b06eaee49f8951b7bb96fb9dd
JOEY RICKSON <jrickson@bajajauto.co.in>
2026-09-25 08:36:13 -0400

    Restaura versao estavel v2.7.1

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE='saldoplan-v2.7.3';
+const CACHE='saldoplan-v2.7.1';
 
 const ASSETS=[
   './',
   './index.html',
-  './styles.css?v=2.7.3',
-  './drive-config.js?v=2.7.3',
-  './app.js?v=2.7.3',
+  './styles.css?v=2.7.1',
+  './drive-config.js?v=2.7.1',
+  './app.js?v=2.7.1',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
